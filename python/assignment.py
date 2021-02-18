1c28d3875c8f8f44680d54d91f39e62a23520ac3
kali <kali@localhost.localdomain>
2021-02-17 19:58:56 -1000

    first commit

---
+++ python/assignment.py
@@ -0,0 +1,6 @@
+ #ASSIGNMENT OPERATOR (=)
+#used to assign a value  to a variable
+
+b = 'This is the value of b'
+
+print(b)
---
+++ python/bitwise.py
@@ -0,0 +1,9 @@
+#BITWISE OPERATORS assume the operands are in binar5 and performs operations on them bit b5 bit
+# retuns 1 or 0 depending on the outcome
+
+print(6 & 3)
+print( ~ 4 )
+
+print(3 | 6)
+print(3 ^ 6)
+
